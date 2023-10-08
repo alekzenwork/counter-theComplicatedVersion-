add3298c9a96f4c7d7c6be1c000dd8191b07382e
alekzenwork <alekzenmusic@gmail.com>
2023-10-09 00:50:53 +0300

    Initial Commit

---
+++ counter(theComplicatedVersion)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  counter(theComplicatedVersion)
+//
+//  Created by Александр Баранов on 09.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
