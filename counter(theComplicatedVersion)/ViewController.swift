62e04ee01414235dc3c9f3aa888b6c41404b3f59
alekzenwork <alekzenmusic@gmail.com>
2023-10-09 13:54:23 +0300

    Исправлен цвет кнопки plusButton на красный при работе приложения. До этого background был синий и становился красным только при нажатии на кнопку plusButton

--- counter(theComplicatedVersion)/ViewController.swift
+++ counter(theComplicatedVersion)/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainCounter: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var historyOfChange: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+private var number = 0
+    @IBAction func plusOne(_ sender: Any) {
+        number += 1
+        mainCounter.text = "\(number)"
+    }
+    
+    @IBAction func minusOne(_ sender: Any) {
+        number -= 1
+        if number > 0 { mainCounter.text = "\(number)"
+        } else {
+            number = 0
+            mainCounter.text = "\(number)"
+        }
+        
+    }
+    @IBAction func resetCounter(_ sender: Any) {
+        number = 0
+        mainCounter.text = "\(number)"
+    }
 }
-
